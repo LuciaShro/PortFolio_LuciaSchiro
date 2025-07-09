1402c2dd64c64858dcb06785c9198136187becf9
Lucia <schirolucia05@gmail.com>
2025-07-09 20:33:25 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+// function mostrarMensaje() {
+//     const Mensaje = document.getElementById('mensaje');
+    
+//     Mensaje.innerHTML = '¡Gracias por tu interes! Me pondre en contacto con vos pronto.';
+//     Mensaje.classList.add('mensaje-exito');
+
+//     document.querySelector('.form-contacto').reset();
+    
+//     setTimeout(function() {
+//         Mensaje.classList.remove('mensaje-exito');
+//         setTimeout(function() {
+//             Mensaje.innerHTML = '';
+//         }, 3000);
+//     }, 3000);
+// }
+
 function mostrarMensaje() {
     const Mensaje = document.getElementById('mensaje');
     
-    Mensaje.innerHTML = '¡Gracias por tu interes! Me pondre en contacto con vos pronto.';
+    Mensaje.innerHTML = '¡Gracias por tu interés! Me pondré en contacto con vos pronto.';
     Mensaje.classList.add('mensaje-exito');
 
-    document.querySelector('.form-contacto').reset();
-    
+    // 🔁 Esperá 1 segundo antes de resetear el formulario
+    setTimeout(function() {
+        document.querySelector('.form-contacto').reset();
+    }, 1000);
+
+    // Ocultar el mensaje después de unos segundos
     setTimeout(function() {
         Mensaje.classList.remove('mensaje-exito');
         setTimeout(function() {
@@ -13,3 +33,4 @@ function mostrarMensaje() {
         }, 3000);
     }, 3000);
 }
+
